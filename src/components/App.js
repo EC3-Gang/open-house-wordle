ca1cbcfc1cdf9e803b571fa3fbbb751cae00cec3
DET171 <mrgeek484@gmail.com>
2022-04-19 20:30:20 +0800

    random stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,58 @@
 import './App.css';
 import React, { Component } from 'react';
+import keyCodes from '../keyCodes';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			guess: '',
+		};
 	}
+
+	handlekeydown = async (event) => {
+		const keycode = event.keyCode || event.charCode;
+		console.log(keycode);
+		if (keycode === 13) {
+			// nothing for now
+		}
+		else if (keycode === 8) {
+			// backspace this.state.guess
+			if (!this.state.guess.length) {
+				return;
+			}
+			await this.setState({
+				guess: this.state.guess.slice(0, -1),
+			});
+		}
+		else if (keyCodes[keycode] !== undefined) {
+			if (keyCodes[keycode] === '[SPACE]') {
+				await this.setState({
+					guess: this.state.guess + ' ',
+				});
+			}
+			else {
+				const val = keyCodes[keycode + ''];
+				this.setState({
+					guess: this.state.guess + val,
+				});
+			}
+		}
+	};
+
+	componentDidMount = () => {
+		console.log('Mounted');
+
+		window.onkeydown = this.handlekeydown;
+	};
 
 	render() {
 		return (
 			<>
 				<h1 className='center'>HCI Open House Worldle</h1>
+				<div className='center'>
+					{this.state.guess}
+				</div>
 			</>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+const App = React.lazy(() => import('./components/App'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<Suspense fallback={<div>Loading...</div>}>
+			<App />
+		</Suspense>
 	</React.StrictMode>,
 );
 

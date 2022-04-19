97f9c1e23f3e24264b9049f1369ff226a84c7b03
DET171 <mrgeek484@gmail.com>
2022-04-19 19:27:17 +0800

    init

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<>
+				<h1 className='center'>HCI Open House Worldle</h1>
+			</>
+		);
+	}
+}
+
+export default App;

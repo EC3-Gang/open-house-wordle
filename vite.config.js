a74e39cbc2fc5fc1c4787c44c2f7870b85cacc57
DET171 <mrgeek484@gmail.com>
2022-05-12 22:03:55 +0800

    ew brotli

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import JSX from '@vitejs/plugin-react';
-import compress from 'vite-plugin-compress';
 
 export default defineConfig({
 	plugins: [
 		splitVendorChunkPlugin(),
 		VitePWA(),
 		JSX(),
-		compress(),
 	],
 });

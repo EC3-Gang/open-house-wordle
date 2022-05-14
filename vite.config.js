a265328552e1e8c1c427903cb6ce1616da245725
DET171 <mrgeek484@gmail.com>
2022-05-14 10:59:17 +0800

    indeed

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import JSX from '@vitejs/plugin-react';
+import Radar from 'vite-plugin-radar';
 
 export default defineConfig({
 	plugins: [
 		splitVendorChunkPlugin(),
 		VitePWA(),
 		JSX(),
+		Radar({
+			analytics: {
+				id: 'G-K2RVE6WEXM',
+			},
+		}),
 	],
 });

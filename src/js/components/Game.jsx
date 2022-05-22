62fd8e491ab25d06305655c1c2e772cabc90e2e7
DET171 <mrgeek484@gmail.com>
2022-05-22 17:52:38 +0800

    e

--- src/js/components/Game.jsx
+++ src/js/components/Game.jsx
@@ -7,6 +7,7 @@ import 'semantic-ui-css/semantic.min.css';
 import places from '../placeList';
 import haversine from '../haversine';
 import Swal from 'sweetalert2';
+import 'sweetalert2/src/sweetalert2.scss';
 
 
 export default class Game extends Component {
